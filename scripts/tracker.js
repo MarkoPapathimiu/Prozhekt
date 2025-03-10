862af23e8e390bd22471b14a420e4d940e7c69ff
MarkoPapathimiu <markopapathimiu@gmail.com>
2025-03-10 22:17:44 +0100

    Marko files

---
+++ scripts/tracker.js
@@ -0,0 +1,25 @@
+// const $userID
+// const $userName;
+// const $password;
+// const $userAge;
+// const $userHeight;
+// const $userWeight;
+// const $userBMI;
+// const $favoriteWorkouts
+// const $favoriteRecipes
+// const $activityLevel
+
+function getUsers(){
+    return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+//Do bejme nje funksion te log in qe ruan nje vlere CurrentUser ne localstorage qe ruan ID e perdoruesit qe u be log in
+//CurrentUser do na ndihmoje te bejme check kush eshte perdoruesi aktual qe te nxjerrim te dhenat e ketij perdoruesi
+function getCurrentUser(){
+
+}
+
+
+workoutID
+workoutName
+workoutDesc
