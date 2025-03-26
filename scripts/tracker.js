0b99206cf56508896f787636853d0d53c5c14d77
MarkoPapathimiu <markopapathimiu@gmail.com>
2025-03-26 23:54:24 +0100

    prettier

--- scripts/tracker.js
+++ scripts/tracker.js
@@ -9,25 +9,23 @@
 // const $favoriteRecipes
 // const $activityLevel
 
-function getUsers(){
-    return JSON.parse(localStorage.getItem("users")) || [];
+function getUsers() {
+  return JSON.parse(localStorage.getItem("users")) || [];
 }
 
-function userProfile(){
-    var user = getUsers();
+function userProfile() {
+  var user = getUsers();
 
-    $('#user-name').text(user.userName);
-    $('#user-age').text(user.userAge);
-    $('#user-height').text(user.userHeight);
-    $('#user-weight').text(user.userWeight);
-    $('#user-bmi').text(user.userBMI);
+  $("#user-name").text(user.userName);
+  $("#user-age").text(user.userAge);
+  $("#user-height").text(user.userHeight);
+  $("#user-weight").text(user.userWeight);
+  $("#user-bmi").text(user.userBMI);
 }
-$(document).ready(function() {
-    userProfile();
+$(document).ready(function () {
+  userProfile();
 });
 
 //Do bejme nje funksion te log in qe ruan nje vlere CurrentUser ne localstorage qe ruan ID e perdoruesit qe u be log in
 //CurrentUser do na ndihmoje te bejme check kush eshte perdoruesi aktual qe te nxjerrim te dhenat e ketij perdoruesi
-function getCurrentUser(){
-
-}
+function getCurrentUser() {}
--- scripts/workout.js
+++ scripts/workout.js
@@ -1,31 +1,31 @@
 const images = document.querySelectorAll(".carousel img");
-      let angle = 0;
-      let currentIndex = 0;
+let angle = 0;
+let currentIndex = 0;
 
-      function updateCarousel() {
-        const radius = 80;
-        const total = images.length;
+function updateCarousel() {
+  const radius = 80;
+  const total = images.length;
 
-        images.forEach((img, index) => {
-          const theta =
-            ((index - currentIndex) * (360 / total) + angle) * (Math.PI / 180);
-          const x = Math.cos(theta) * radius;
-          const y = Math.sin(theta) * radius;
+  images.forEach((img, index) => {
+    const theta =
+      ((index - currentIndex) * (360 / total) + angle) * (Math.PI / 180);
+    const x = Math.cos(theta) * radius;
+    const y = Math.sin(theta) * radius;
 
-          img.style.transform = `translate(${x}px, ${y}px)`;
-          img.classList.remove("active");
-        });
+    img.style.transform = `translate(${x}px, ${y}px)`;
+    img.classList.remove("active");
+  });
 
-        images[currentIndex].classList.add("active");
-      }
+  images[currentIndex].classList.add("active");
+}
 
-      function rotateCarousel() {
-        currentIndex = (currentIndex + 1) % images.length;
-        updateCarousel();
-      }
+function rotateCarousel() {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateCarousel();
+}
 
-      images.forEach((img) => {
-        img.addEventListener("click", rotateCarousel);
-      });
+images.forEach((img) => {
+  img.addEventListener("click", rotateCarousel);
+});
 
-      updateCarousel();
+updateCarousel();
